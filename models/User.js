001917fd5414e1dbc8105549237bd14d09e80389
Peter Watkins <pdwatkins@me.com>
2016-04-10 19:33:00 -0700

    updates

--- models/User.js
+++ models/User.js
@@ -45,23 +45,22 @@ UserSchema.pre('save', function(next) {
     var self = this;
 
     // only hash the password if it has been modified (or is new)
-    if (!self.isModified('password')) {
-      console.log('password not modified so return next');
-      return next();
-    }
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+    if (self.isModified('password')) {
+      console.log('password modified so do the salt thing');
+      // generate a salt
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+          if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(self.password, salt, function(err, hash) {
-            if (err) return next(err);
+          // hash the password using our new salt
+          bcrypt.hash(self.password, salt, function(err, hash) {
+              if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            self.password = hash;
-            next();
-        });
-    });
+              // override the cleartext password with the hashed one
+              self.password = hash;
+              next();
+          });
+      });
+    };
 
     if (!self.authyId) {
         // Register this user if it's a new user
