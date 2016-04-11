113ea09b73ace32c66bac8793c125dfec5b155b6
Peter Watkins <pdwatkins@me.com>
2016-04-11 16:23:01 -0700

    updates

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ var middleware = require('./middleware');
 var users = require('./users');
 var sessions = require('./sessions');
 var bodyParser = require('body-parser');
+var urlEncodedParser = require('body-parser').urlencoded({ extended: false});
 
 // Mount API routes on the Express web app
 module.exports = function(app) {
@@ -9,26 +10,26 @@ module.exports = function(app) {
     app.use(middleware.loadUser);
 
     // Create a new user
-    app.post('/user', bodyParser.urlencoded(), users.create);
+    app.post('/user', urlEncodedParser, users.create);
 
     // Get information about the currently logged in user
-    app.get('/user', bodyParser.urlencoded(), middleware.loginRequired, users.getUser);
+    app.get('/user', middleware.loginRequired, users.getUser);
 
     // Create a new session
-    app.post('/session', bodyParser.urlencoded(), sessions.create);
+    app.post('/session', urlEncodedParser, sessions.create);
 
     // Log out (destroy a session)
-    app.delete('/session', bodyParser.urlencoded(), middleware.loginRequired, sessions.destroy);
+    app.delete('/session', urlEncodedParser, middleware.loginRequired, sessions.destroy);
 
     // Check the OneTouch status on the user
-    app.get('/authy/status', bodyParser.urlencoded(), sessions.authyStatus);
+    app.get('/authy/status', sessions.authyStatus);
 
     // The webhook that Authy will call on a OneTouch event
     app.post('/authy/callback', bodyParser.json(), middleware.validateSignature, sessions.authyCallback);
 
     // Validate the given session with an Authy 2FA token
-    app.post('/session/verify', bodyParser.urlencoded(), sessions.verify);
+    app.post('/session/verify', urlEncodedParser, sessions.verify);
 
     // resend an authorization token
-    app.post('/session/resend', bodyParser.urlencoded(), sessions.resend);
+    app.post('/session/resend', urlEncodedParser, sessions.resend);
 };
